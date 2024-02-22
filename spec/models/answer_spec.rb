44e0e504482b289f29951350e079f2eaa5049daf
Runidan <rusin.dn+github@gmail.com>
2024-02-22 11:36:43 +0500

    Added tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  it { should belong_to(:question) }
+
+  it { should have_db_column(:body).of_type(:text) }
   it { should validate_presence_of(:body) }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  it { should have_many(:answers).dependent(:destroy) } 
+
+  it { should have_db_column(:title).of_type(:string) }
   it { should validate_presence_of(:title) }
+  it { should have_db_column(:body).of_type(:text) }
   it { should validate_presence_of(:body) }
 end
