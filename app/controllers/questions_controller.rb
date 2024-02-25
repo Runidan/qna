869b06e1b44c58cdf272581845addade0afc6062
Runidan <rusin.dn+github@gmail.com>
2024-02-25 20:41:50 +0500

    Tests for get requests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,24 @@
 class QuestionsController < ApplicationController
+before_action :load_question, only: [:edit, :show]
+
   def index
     @questions = Question.all    
   end
 
   def show
-    @question = Question.find params[:id]
   end
 
   def new
     @question = Question.new
   end
+
+  def edit
+  end
+  
+  private
+  
+  def load_question
+    @question = Question.find params[:id]    
+  end
+  
 end
