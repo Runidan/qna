9f860820eba030688510e552d9668797ae03385a
Runidan <rusin.dn+github@gmail.com>
2024-03-02 23:08:31 +0500

    devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :questions do
     resources :answers, shallow: true
   end
+
+  root to: "home#index"
 end
