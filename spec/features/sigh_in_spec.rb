1ff2e3978b8e7d159ed5a617d2b3ce99db9e588c
Runidan <rusin.dn+github@gmail.com>
2024-03-05 13:09:37 +0500

    Video part 1

--- spec/features/sigh_in_spec.rb
+++ spec/features/sigh_in_spec.rb
@@ -5,15 +5,24 @@ feature "User can sing in", %q{
   As an unauthenticated user
   I'd like to be able to sigh in
 } do
-  scenario 'Registered user tries to sigh in' do
-    User.create!(email: 'user@test.com', password: '12345678')
 
-    visit new_user_session_path
-    fill_in 'Email', with: 'user@test.ru' 
-    fill_in 'Password', with: '12345678'
+  given(:user) { User.create!(email: 'user@test.com', password: '12345678')}
+
+  background { visit new_user_session_path }
+
+  scenario 'Registered user tries to sigh in' do
+    fill_in 'Email', with: user.email 
+    fill_in 'Password', with: user.password
+    click_on "Log in"
 
     expect(page).to have_content 'Signed in successfully.'
   end
 
-  scenario 'Unregistered user tries to sigh in'
+  scenario 'Unregistered user tries to sigh in' do
+    fill_in 'Email', with: 'wrong@test.com' 
+    fill_in 'Password', with: '12345678'
+    click_on "Log in"
+
+    expect(page).to have_content "Invalid Email or password." 
+  end
 end
