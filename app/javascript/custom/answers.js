5dd4b85858be212be288531f46ee09a28458cb29
Runidan <rusin.dn+github@gmail.com>
2024-03-24 09:13:56 +0500

    changed delete answer

--- app/javascript/custom/answers.js
+++ app/javascript/custom/answers.js
@@ -5,17 +5,4 @@ $(document).on('turbolinks:load', function(){
     var answerId = $(this).data('answerId')
     $('form#edit-answer-' + answerId).removeClass('hidden');
   })
-
-  $('.answers-list').on('click', '.delete-answer-link', function(e) {
-    e.preventDefault();
-    var answerId = $(this).data('answerId');
-    
-    if (confirm('Are you sure you want to delete this answer?')) {
-      $.ajax({
-        url: '/answers/' + answerId,
-        type: 'DELETE',
-        dataType: 'script'
-      });
-    }
-  })
 });
