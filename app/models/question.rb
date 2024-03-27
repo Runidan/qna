d6d529935fafa4aec419bed3c79bdb708b3852a6
Runidan <rusin.dn+github@gmail.com>
2024-03-27 14:28:07 +0500

    after lessons

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,6 +5,8 @@ class Question < ApplicationRecord
   belongs_to :best_answer, class_name: 'Answer', optional: true
   belongs_to :user, foreign_key: :user_id
 
+  has_many_attached :files
+
   validates :title, :body, presence: true
   validates :user, presence: true, associated: { class: User }
   validates :best_answer_id, numericality: { only_integer: true, allow_nil: true }
