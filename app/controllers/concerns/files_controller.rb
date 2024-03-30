0cf71063afba134df5f7fe76e0d99e5462c91c5a
Runidan <rusin.dn+github@gmail.com>
2024-03-30 16:42:50 +0500

    fixed problem with file destroy

--- app/controllers/concerns/files_controller.rb
+++ app/controllers/concerns/files_controller.rb
@@ -1,9 +1,8 @@
 class FilesController < ApplicationController
   def destroy
     @file = ActiveStorage::Attachment.find(params[:id])
-    @file_deleted = false
     if current_user&.author_of?(@file.record)
-      @file_deleted = @file.purge
+      @file.purge
     else
       head :forbidden
     end
