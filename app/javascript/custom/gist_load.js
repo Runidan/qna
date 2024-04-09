09631f1a990816d26650150049ccac97beb78c38
Runidan <rusin.dn+github@gmail.com>
2024-04-09 15:55:05 +0500

    delete comments

--- app/javascript/custom/gist_load.js
+++ app/javascript/custom/gist_load.js
@@ -4,22 +4,17 @@ $(document).on('turbolinks:load', function() {
     var $this = $(this);
     var href = $this.attr('href');
     
-    // Проверяем, что ссылка ведёт на gist.github.com
     if (href && href.includes('gist.github.com')) {
-      // Создаём контейнер для содержимого Gist
       var gistContainer = $('<div class="gist-content"></div>');
       $this.after(gistContainer);
 
       $.ajax({
-        url: href + '.json', // Получить JSON версию Gist
+        url: href + '.json',
         method: 'GET',
-        dataType: 'jsonp', // Использовать JSONP для обхода политики same-origin
+        dataType: 'jsonp',
         success: function(gistData) {
-          // Gist может быть добавлен сразу, вставив HTML-код
           gistContainer.html(gistData.div);
           
-          // Дополнительно добавляем скрипты, если они есть,
-          // чтобы форматирование и стили Gist отображались корректно.
           if (gistData.stylesheet && !$('link[href="' + gistData.stylesheet + '"]').length) {
             var linkTag = $('<link>')
               .attr('rel', 'stylesheet')
@@ -28,7 +23,6 @@ $(document).on('turbolinks:load', function() {
           }
         },
         error: function() {
-          // Обработка ошибок, если Gist загрузить не удалось
           gistContainer.html('<p>Error loading Gist</p>');
         }
       });
